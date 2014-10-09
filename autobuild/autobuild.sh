ca43f6c5d747e4b12de5a9203b2c7dcce5f151e5
Stefan Dang <sd15@sanger.ac.uk>
2014-10-09 14:38:35 +0100

    Pushing autobuild for review

--- autobuild/autobuild.sh
+++ autobuild/autobuild.sh
@@ -10,9 +10,9 @@
 set -e
 
 # GLOBALS
-PROG=$0; shift                    # $0
-INPUT=$0; shift                   # $1 input directory on local machine
-OUTPUT=$0; shift                  # $2 output directory on local machine
+PROG=$0
+INPUT=$1                 # input directory on local machine
+OUTPUT=$2                # output directory on local machine
 
 
 # FUNCTIONS
@@ -28,7 +28,7 @@ function docker::build_and_run {
     docker run --rm -v "$OUTPUT:/autobuild/" "sanger_npg/autobuild-$dir" && \
     echo "sanger_npg/autobuild-$dir successfully built."
   else
-    echo "Skipped $dir: $folder/Dockerfile not present."
+    echo "$dir skipped: $folder/Dockerfile not present."
     return
   fi
 }
@@ -37,7 +37,7 @@ function docker::build_and_run {
 # MAIN
 function main {
   if [ $# -le 0 ]; then
-    err "USAGE: $PROG <output dir> <optional: specific targets>"
+    err "USAGE: $PROG <input dir> <output dir>"
   fi
 
   # Iterate over all / selected folders
